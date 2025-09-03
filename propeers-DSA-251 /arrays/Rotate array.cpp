3c3227173c1f1b29badc8e7affaba253d869539b
Rajanala Manohar Reddy <manoharreddyrajanala@gmail.com>
2025-09-03 23:27:25 +0530

    Update Rotate array.cpp

--- propeers-DSA-251 /arrays/Rotate array.cpp
+++ propeers-DSA-251 /arrays/Rotate array.cpp
@@ -15,7 +15,6 @@ vector<int> rotateArray(vector<int>arr, int k) {
 // Given an array 'arr' with 'n' elements, the task is to rotate the array to the left by 'k' steps, where 'k' is non-negative.
 
 
-
 // Example:
 // 'arr '= [1,2,3,4,5]
 // 'k' = 1  rotated array = [2,3,4,5,1]
